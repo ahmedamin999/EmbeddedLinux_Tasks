a34a8c08f39c83b7b6d4748d72c5146c4a149056
Ahmed Amin <ahmedamin9@hotmail.com>
2023-08-21 21:47:22 +0300

    Session 1 Task 02
    Python program to test whether a passed letter is a vowel or not.

---
+++ Session1/S1Task02.py
@@ -0,0 +1,7 @@
+char = input ("Input the letter to check:")
+vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']  
+
+if char in vowel:
+    print (f"This Character  {char}  is Vowel")
+else:
+    print (f"This Character {char} is NOT Vowel")
